b7818ada205b2dfed9bc6357c6215c6368c30454
wingjson <wingjson@outlook.com>
2024-03-28 18:39:46 +0800

    build test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    // setGreetMsg(await invoke("convert", { name, target_type: 3 }));
+    setGreetMsg(await invoke("convert", { name, target_type: 3 }));
     const result = await invoke("convert", { source:name, targetType: 3 });
     console.log(result);
   }
